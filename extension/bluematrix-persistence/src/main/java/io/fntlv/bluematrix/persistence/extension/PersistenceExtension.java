01a94877c150e91873dd3cdcde2d6cef85c95697
ailanlana <1781872216@qq.com>
2026-06-30 17:53:29 +0800

    build(persistence): update everydatabase manager dependencies

--- extension/bluematrix-persistence/src/main/java/io/fntlv/bluematrix/persistence/extension/PersistenceExtension.java
+++ extension/bluematrix-persistence/src/main/java/io/fntlv/bluematrix/persistence/extension/PersistenceExtension.java
@@ -6,27 +6,28 @@ import io.fntlv.bluematrix.core.extension.BlueMatrixExtensionContext;
 import io.fntlv.bluematrix.loader.library.BlueLibraryFactory;
 
 public final class PersistenceExtension implements BlueMatrixExtension {
-    private static final String EVERY_DATABASE_CORE = "br.com.finalcraft.everydatabase:everydatabase-core:1.0.4";
+    private static final String EVERY_DATABASE_CORE = "br.com.finalcraft.everydatabase:everydatabase-core:1.0.5";
+    private static final String EVERY_DATABASE_MANAGER = "br.com.finalcraft.everydatabase:everydatabase-manager:1.0.5";
     private static final String HIKARI_CP = "com.zaxxer:HikariCP:4.0.3";
     private static final String HIKARI_PACKAGE = "com.zaxxer.hikari";
     private static final String RELOCATED_HIKARI_PACKAGE = "io.fntlv.bluematrix.persistence.libs.hikari";
 
     private static final String[] EVERY_DATABASE_RUNTIME_LIBRARIES = {
-            "com.fasterxml.jackson.core:jackson-core:2.15.4",
-            "com.fasterxml.jackson.core:jackson-annotations:2.15.4",
-            "com.fasterxml.jackson.core:jackson-databind:2.15.4",
-            "com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.15.4",
-            "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.4",
-            "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.4",
-            "org.yaml:snakeyaml:2.1",
+            "com.fasterxml.jackson.core:jackson-core:2.22.0",
+            "com.fasterxml.jackson.core:jackson-annotations:2.22.0",
+            "com.fasterxml.jackson.core:jackson-databind:2.22.0",
+            "com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.22.0",
+            "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.22.0",
+            "com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.22.0",
+            "org.yaml:snakeyaml:2.6",
             "org.slf4j:slf4j-api:1.7.36",
             "com.h2database:h2:1.4.200",
-            "org.mongodb:mongodb-driver-sync:4.11.2",
-            "org.mongodb:mongodb-driver-core:4.11.2",
-            "org.mongodb:bson:4.11.2",
-            "org.mongodb:bson-record-codec:4.11.2",
-            "com.mysql:mysql-connector-j:9.4.0",
-            "org.postgresql:postgresql:42.7.7"
+            "org.mongodb:mongodb-driver-sync:5.8.0",
+            "org.mongodb:mongodb-driver-core:5.8.0",
+            "org.mongodb:bson:5.8.0",
+            "org.mongodb:bson-record-codec:5.8.0",
+            "com.mysql:mysql-connector-j:9.7.0",
+            "org.postgresql:postgresql:42.7.12"
     };
 
     @Override
@@ -39,6 +40,11 @@ public final class PersistenceExtension implements BlueMatrixExtension {
                         BlueLibraryFactory.of(EVERY_DATABASE_CORE)
                                 .relocate(HIKARI_PACKAGE, RELOCATED_HIKARI_PACKAGE),
                         "br.com.finalcraft.everydatabase.Storage"
+                )
+                .extensionLibrary(
+                        context.getName(),
+                        BlueLibraryFactory.of(EVERY_DATABASE_MANAGER),
+                        "br.com.finalcraft.everydatabase.manager.CachingManager"
                 )
                 .extensionLibrary(
                         context.getName(),
